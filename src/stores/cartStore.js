42edd355afb72981a7b03999188030c0844798a9
zh <1399367990@qq.com>
2025-03-27 12:44:51 +0800

    修改了一下购物车的删除功能，变更为只删除选中的商品

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -25,12 +25,17 @@ export const useCartStore = defineStore('cart', () => {
   }
 
     // 删除购物车
-     const delCart = (name) => {
-       const idx = cartList.value.findIndex((item) => name === item.name)
-      cartList.value.splice(idx, 1)
-      cartList.value = []; // 清空购物车
-      isAll.value = false;
-    }
+// 删除购物车中的单个商品
+    const delCart = (name) => {
+        const idx = cartList.value.findIndex((item) => name === item.name);
+        if (idx !== -1) {
+            cartList.value.splice(idx, 1); // 仅删除匹配的商品
+        }
+
+        // 如果购物车为空，取消全选状态
+        isAll.value = cartList.value.length > 0;
+    };
+
 
     //单选功能
     const singleCheck = (name,selected) => {

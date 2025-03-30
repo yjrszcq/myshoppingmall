88241b448b45e6a69edbd79a6a1d0eb2ff96d6dc
Estrella0101 <1696770972@qq.com>
2025-03-31 02:06:11 +0800

    完成购物接口实现？

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,113 +1,134 @@
 //封装购物车模块
 
 import { defineStore } from 'pinia'
-import { computed,ref} from 'vue'
+import { computed, ref } from 'vue'
 import { useUserstore } from './user'
-import { addItemToCartAPI,getCartContentsAPI,deleteCartItemAPI,updateCartItemQuantityAPI } from '@/apis/cart'
+import { addItemToCartAPI, getCartContentsAPI, deleteCartItemAPI, updateCartItemQuantityAPI } from '@/apis/cart'
+import { ElMessage } from 'element-plus'
 
 export const useCartStore = defineStore('cart', () => {
-    const userStore = useUserstore()
-    const isLogin = computed(() => userStore.userInfo.token)
+  const userStore = useUserstore()
+  const isLogin = computed(() => userStore.userInfo.token)
 
-    //定义cartlist 
-    const cartList = ref([])
-   
- 
-    //定义添加
-    const addCart =  async (goods)=>{
-      const { productId, quantity } = goods
-      if(isLogin.value){
-        //已登录后的逻辑
-        await addItemToCartAPI({productId, quantity})
-        const res = await getCartContentsAPI()
-        cartList.value = res.result
-      }
-      else{
-        // 添加购物车操作
-        const item = cartList.value.find((item) => goods.productId === item.productId)
-      if(item){
-        item.quantity+=goods.quantity
-      }
-      else{
-        cartList.value.push(goods)
-      }
-      }
+  //定义cartlist 
+  const cartList = ref([])
+
+
+  const getCart = async () => {
+    const res = await getCartContentsAPI();
+    cartList.value = res.items
+  };
+
+
+  //定义添加
+  const addCart = async (goods) => {
+    const { productId, quantity } = goods
+    // if (isLogin.value) {
+    // 已登录后的逻辑
+    await addItemToCartAPI({ productId, quantity })
+    const res = await getCartContentsAPI()
+    cartList.value = res.items
+    // }
+    // else {
+    //   // 添加购物车操作
+    //   const item = cartList.value.find((item) => goods.productId === item.productId)
+    //   if (item) {
+    //     item.quantity += goods.quantity
+    //   }
+    //   else {
+    //     cartList.value.push(goods)
+    //   }
+    // }
+
+    ElMessage({
+      message: 'Added successfully.',
+      type: 'success',
+    })
   }
 
-    // 删除购物车
-     const delCart = async (productId) => {
-      if(isLogin.value){
-        //调用接口
-        await deleteCartItemAPI(productId)
-        const res = await getCartContentsAPI()
-        cartList.value = res.result
+  // 删除购物车
+  const delCart = async (productId) => {
+    console.log('删除购物车', productId);
 
-      }
-      else{
-        const idx = cartList.value.findIndex((item) => productId === item.productId)
-        cartList.value.splice(idx, 1)
-        isAll.value = false;
-      }
+    // if (isLogin.value) {
+    //调用接口
+    await deleteCartItemAPI(productId)
+    const res = await getCartContentsAPI()
+    cartList.value = res.items
+
+    // }
+    // else {
+    //   const idx = cartList.value.findIndex((item) => productId === item.productId)
+    //   cartList.value.splice(idx, 1)
+    //   isAll.value = false;
+    // }
+
+    ElMessage({
+      message: 'Delete successfully.',
+      type: 'success',
+    })
+  }
+
+  //更新商品数量
+  const updateQuantity = async (itemId, quantity) => {
+    // if (isLogin.value) {
+    // 已登录后的逻辑
+    await updateCartItemQuantityAPI(itemId, quantity)
+    const res = await getCartContentsAPI()
+    // console.log(res);
+    cartList.value = res.items
+    // } else {
+    //   // 未登录的逻辑
+    //   const item = cartList.value.find((item) => itemId === item.itemId)
+    //   if (item) {
+    //     item.quantity = quantity
+    //   }
+    // }
+  }
+
+
+
+  //单选功能
+  const singleCheck = (productId, selected) => {
+    const item = cartList.value.find((item) => item.productId === productId)
+    item.selected = selected
+  }
+
+
+  //  总的数量 所有项的count之和
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.quantity, 0))
+
+  //是否全选
+  const isAll = computed(() => {
+    if (cartList.value.length === 0) {
+      return false; // 如果购物车为空，返回 false
     }
+    return cartList.value.every((item) => item.selected);
+  });
 
-      //更新商品数量
-      const updateQuantity = async (itemId, quantity) => {
-        if(isLogin.value){
-          // 已登录后的逻辑
-          await updateCartItemQuantityAPI(itemId, quantity)
-          const res = await getCartContentsAPI()
-          cartList.value = res.result
-        } else {
-          // 未登录的逻辑
-          const item = cartList.value.find((item) => itemId === item.itemId)
-          if(item) {
-            item.quantity = quantity
-          }
-        }
-      }
+  //全选功能
+  const allCheck = (selected) => {
+    cartList.value.forEach(item => item.selected = selected)
+  }
 
+  //已选择数量
+  const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.quantity, 0))
+  //已选择合计
+  const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.quantity * c.price, 0))
 
-
-    //单选功能
-    const singleCheck = (productId,selected) => {
-      const item = cartList.value.find((item) => item.productId===productId)
-      item.selected = selected
-    }
-
-
-    //  总的数量 所有项的count之和
-    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.quantity, 0))
-
-    //是否全选
-    const isAll = computed(() => {
-      if (cartList.value.length === 0) {
-        return false; // 如果购物车为空，返回 false
-      }
-      return cartList.value.every((item) => item.selected);
-    });
-
-    //全选功能
-    const allCheck = (selected) =>{
-      cartList.value.forEach(item => item.selected = selected)
-    }
-
-    //已选择数量
-    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.quantity, 0))
-    //已选择合计
-    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.quantity * c.price, 0))
-
-    return{
-        cartList,
-        addCart,
-        isAll,
-        allCheck,
-        singleCheck,
-        delCart,
-        updateQuantity,
-        selectedCount,
-        selectedPrice,
-        allCount
-    }
-},{
-    persist: true,
+  return {
+    cartList,
+    addCart,
+    isAll,
+    allCheck,
+    singleCheck,
+    delCart,
+    updateQuantity,
+    selectedCount,
+    selectedPrice,
+    allCount,
+    getCart
+  }
+}, {
+  persist: true,
 })
--- src/views/Cart/index.vue
+++ src/views/Cart/index.vue
@@ -1,60 +1,79 @@
 <script setup>
-import { useCartStore } from '@/stores/cartStore'
-import { ElMessage } from 'element-plus'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { useCartStore } from "@/stores/cartStore";
+import { ElMessage } from "element-plus";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { getCartContentsAPI } from "@/apis/cart.js";
+import { storeToRefs } from "pinia";
 
-const cartStore = useCartStore()
-const router = useRouter()
+const cartStore = useCartStore();
+const router = useRouter();
+
+const { cartList } = storeToRefs(cartStore);
+const { getCart, updateQuantity, delCart } = cartStore;
 
 const singleCheck = (productId, selected) => {
-  const item = cartStore.cartList.find((item) => item.productId === productId)
+  const item = cartStore.cartList.find((item) => item.productId === productId);
   if (item) {
-    item.selected = selected
+    item.selected = selected;
   }
-}
+};
 
-const allCheck = (selected)=> {
-  cartStore.allCheck(selected)
-}
+const allCheck = (selected) => {
+  cartStore.allCheck(selected);
+};
 
 // 下单结算
 const checkout = () => {
   // 保存选中的商品信息到本地存储
-  const selectedGoods = cartStore.cartList.filter(item => item.selected)
+  const selectedGoods = cartStore.cartList.filter((item) => item.selected);
   if (selectedGoods.length === 0) {
+    ElMessage.warning("Please select the product you want to check");
 
-    ElMessage.warning('Please select the product you want to check')
-
-    return
+    return;
   }
-  
+
   // 计算商品总价和应付总额
-  const totalPrice = selectedGoods.reduce((sum, item) => sum + item.price * item.count, 0)
-  const postFee = 10 // 运费
-  const totalPayPrice = totalPrice + postFee
-  
+  const totalPrice = selectedGoods.reduce(
+    (sum, item) => sum + item.price * item.count,
+    0
+  );
+  const postFee = 10; // 运费
+  const totalPayPrice = totalPrice + postFee;
+
   // 将选中的商品信息和金额信息保存到本地存储
-  localStorage.setItem('cartInfo', JSON.stringify({
-    goods: selectedGoods.map(item => ({
-      id: item.id,
-      name: item.name,
-      picture: item.picture,
-      price: item.price,
-      count: item.count,
-      attrsText: item.attrsText
-    })),
-    summary: {
-      goodsCount: selectedGoods.reduce((sum, item) => sum + item.count, 0),
-      totalPrice: totalPrice,
-      postFee: postFee,
-      totalPayPrice: totalPayPrice
-    }
-  }))
-  
+  localStorage.setItem(
+    "cartInfo",
+    JSON.stringify({
+      goods: selectedGoods.map((item) => ({
+        id: item.id,
+        name: item.name,
+        picture: item.picture,
+        price: item.price,
+        count: item.count,
+        attrsText: item.attrsText,
+      })),
+      summary: {
+        goodsCount: selectedGoods.reduce((sum, item) => sum + item.count, 0),
+        totalPrice: totalPrice,
+        postFee: postFee,
+        totalPayPrice: totalPayPrice,
+      },
+    })
+  );
+
   // 跳转到结算页面
-  router.push('/checkout')
-}
+  router.push("/checkout");
+};
+
+const onChange = (item) => {
+  const { itemId, quantity } = item;
+  updateQuantity(itemId, quantity);
+};
+
+onMounted(() => {
+  getCart();
+});
 </script>
 
 <template>
@@ -63,92 +82,109 @@ const checkout = () => {
       <div class="cart">
         <table>
           <thead>
-          <tr>
-            <th width="120">
-              <el-checkbox :model-value="cartStore.isAll" @change="allCheck"/>
-            </th>
+            <tr>
+              <th width="120">
+                <el-checkbox
+                  :model-value="cartStore.isAll"
+                  @change="allCheck"
+                />
+              </th>
 
-            <th width="400">Product information</th>
-            <th width="220">unit price</th>
-            <th width="180">quantity</th>
-            <th width="180">total</th>
-            <th width="140">operation</th>
-
-          </tr>
+              <th width="400">Product information</th>
+              <th width="220">unit price</th>
+              <th width="180">quantity</th>
+              <th width="180">total</th>
+              <th width="140">operation</th>
+            </tr>
           </thead>
           <!-- 商品列表 -->
           <tbody>
-          <tr v-for="i in cartStore.cartList" :key="i.id">
-            <td>
+            <tr v-for="i in cartStore.cartList" :key="i.id">
+              <td>
                 <!-- 单选框 -->
-              <el-checkbox :model-value="i.selected" @change="(selected)=>singleCheck(i.productId,selected)" />
-            </td>
-            <td>
-              <div class="goods">
-                <RouterLink to="/"><img :src="i.picture" alt="" /></RouterLink>
-                <div>
-                  <p class="name ellipsis">
-                    {{ i.name }}
-                  </p>
+                <el-checkbox
+                  :model-value="i.selected"
+                  @change="(selected) => singleCheck(i.productId, selected)"
+                />
+              </td>
+              <td>
+                <div class="goods">
+                  <RouterLink to="/"
+                    ><img :src="i.picture" alt=""
+                  /></RouterLink>
+                  <div>
+                    <p class="name ellipsis">
+                      {{ i.name }}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td class="tc">
-              <p>&yen;{{ i.price }}</p>
-            </td>
-            <td class="tc">
-              <el-input-number v-model="i.quantity":min="1" />
-            </td>
-            <td class="tc">
-              <p class="f16 red">&yen;{{ (i.price * i.quantity).toFixed(2) }}</p>
-            </td>
-            <td class="tc">
-              <p>
-
-                <el-popconfirm title="Do you confirm the deletion?" confirm-button-text="yes" cancel-button-text="no" @confirm="cartStore.delCart(i.name)">
-                  <template #reference>
-                    <a href="javascript:;">delete</a>
-
-                  </template>
-                </el-popconfirm>
-              </p>
-            </td>
-          </tr>
-          <tr v-if="cartStore.cartList.length === 0">
-            <td colspan="6">
-              <div class="cart-none">
-
-                <el-empty description="The cart list is empty">
-                  <el-button style="background-color: #ff66b3;color: white" @click="$router.push('/')">Feel free to take a look</el-button>
-
-                </el-empty>
-              </div>
-            </td>
-          </tr>
+              </td>
+              <td class="tc">
+                <p>&yen;{{ i.price }}</p>
+              </td>
+              <td class="tc">
+                <el-input-number
+                  @change="onChange(i)"
+                  v-model="i.quantity"
+                  :min="1"
+                />
+              </td>
+              <td class="tc">
+                <p class="f16 red">
+                  &yen;{{ (i.price * i.quantity).toFixed(2) }}
+                </p>
+              </td>
+              <td class="tc">
+                <p>
+                  <el-popconfirm
+                    title="Do you confirm the deletion?"
+                    confirm-button-text="yes"
+                    cancel-button-text="no"
+                    @confirm="delCart(i.itemId)"
+                  >
+                    <template #reference>
+                      <a href="javascript:;">delete</a>
+                    </template>
+                  </el-popconfirm>
+                </p>
+              </td>
+            </tr>
+            <tr v-if="cartStore.cartList.length === 0">
+              <td colspan="6">
+                <div class="cart-none">
+                  <el-empty description="The cart list is empty">
+                    <el-button
+                      style="background-color: #ff66b3; color: white"
+                      @click="$router.push('/')"
+                      >Feel free to take a look</el-button
+                    >
+                  </el-empty>
+                </div>
+              </td>
+            </tr>
           </tbody>
-
         </table>
       </div>
       <!-- 操作栏 -->
       <div class="action">
         <div class="batch">
-
-          Total {{cartStore.allCount}} products, {{cartStore.selectedCount}} selected, total:
-          <span class="red">¥ {{cartStore.selectedPrice.toFixed(2)}} </span>
+          Total {{ cartStore.allCount }} products,
+          {{ cartStore.selectedCount }} selected, total:
+          <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
         </div>
         <div class="total">
-          <el-button size="large" style="background-color: #ff66b3;color: white" @click="checkout">Place an order for settlement</el-button>
-
+          <el-button
+            size="large"
+            style="background-color: #ff66b3; color: white"
+            @click="checkout"
+            >Place an order for settlement</el-button
+          >
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </div>
   </div>
 </template>
-
-
-
 
 <style scoped lang="scss">
 .xtx-cart-page {
@@ -228,7 +264,7 @@ const checkout = () => {
       height: 100px;
     }
 
-    >div {
+    > div {
       width: 280px;
       font-size: 16px;
       padding-left: 10px;
@@ -273,7 +309,5 @@ const checkout = () => {
     font-weight: normal;
     line-height: 50px;
   }
-
 }
-
 </style>

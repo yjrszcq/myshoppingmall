9493eed0c1bf7a0f29202bc3dda50ed7f1cc2e83
zh <1399367990@qq.com>
2025-04-17 13:31:26 +0800

    修改了发送请求时的错误

--- src/apis/promotionInfo.js
+++ src/apis/promotionInfo.js
@@ -2,5 +2,5 @@
 import httpInstance from "@/utils/http";
 
 export const getPromotionByProductId = (productId) => {
-  return httpInstance.get("/api/products/${productId}/promotion");
+  return httpInstance.get(`/api/products/${productId}/promotion`);
 };
